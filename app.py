61262cf5d66a31c887ea2e6b88cd73b76c78545a
Fernando Melo <fernando.omelo@hotmail.com>
2024-11-13 19:56:50 -0300

    Aula 02

--- app.py
+++ app.py
@@ -1,1 +1,41 @@
-print('Hello World!')
+import os
+
+def fechar_app():
+    os.system('cls')
+    print('Encerrando o programa\n')
+
+def imprime_cabecalho():
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def escolher_opcoes():
+    opcao_selecionada = int(input('Digite a opção desejada: '))
+
+    if opcao_selecionada == 1:
+        print('Cadastrar restaurante')
+    elif opcao_selecionada == 2:
+        print('Listar restaurantes')
+    elif opcao_selecionada == 3:
+        print('Ativar restaurante')
+    else:
+        fechar_app()
+
+def main():
+    imprime_cabecalho()
+    exibir_opcoes()
+    escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
